34c7cc699720ff1cb21c06335bf1b6d7abe018fc
Jackyspractice <s10828152@cycu.edu.tw>
2022-03-25 19:41:40 +0800

    input -t utf8, input function

--- 上機＃2/DS2ex2_10828152.cpp
+++ 上機＃2/DS2ex2_10828152.cpp
@@ -1,23 +1,26 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
 
 using namespace std;
 
 class collegeType{
     public:
         string number;//serial
-        string school;
-        string department;
-        string dayclub;
-        string level;
-        string sNO;//nuber of students
-};collegeType collegeType;
+        string school;//2
+        string department;//4
+        string dayclub;//5
+        string level;//6
+        string sNO;//7nuber of students
+};
 
 int main(){
 
     while(1){
         //which
         int which = 777;
-        cout << "(Stop, 23Tree, AVL) = (0, 1, 2)\n";
+        cout << "哈囉(Stop, 23Tree, AVL) = (0, 1, 2)\n";
         cin >> which;
         if (which == 0) return 0;
         else if (which != 1 && which != 2 && which != 3){
@@ -25,11 +28,29 @@ int main(){
             continue;
         }
         //read file
-        
+        cout << "Please input file name...\n";
+        ifstream file;
+        string fileinput = "input";
+        string filenumber;
+        cin >> filenumber;
+        string fileopen =  fileinput.append(filenumber);
+        fileopen = fileopen.append(".txt");
+        file.open(fileopen);
+        vector<string> buffer;
+
+        if(!file.is_open()){
+            cout << "fail to open file\n";
+        }
+
+        else{
+            string line;
+            for(int i = 0; i < 3; i++) getline(file, line);
+            while(!file.eof()){
+                getline(file, line, '\t');
+                buffer.push_back(line);
+                cout << line << "\n";
+            }
+            file.close();
+        }
     }
-
-    cin >> collegeType.sNO;
-    cout << collegeType.sNO;
-
-
 }
