1b622f47161af914e589093252090ecb2596916b
Jackyspractice <s10828152@cycu.edu.tw>
2022-03-27 13:51:21 +0800

    comment cout

--- 上機＃2/DS2ex2_10828152.cpp
+++ 上機＃2/DS2ex2_10828152.cpp
@@ -95,7 +95,7 @@ int main(){
                 collegeType input[size];
                 node* root = NULL;
                 for(int i = 0; i < size; i++){
-                    cout << "input " << i + 1 << " data\n";
+                    //cout << "input " << i + 1 << " data\n";
                     store(i, buffer, input[i]);
                     //cout << "insert input in root...\n";
                     root = insert(input[i], root);
@@ -122,35 +122,35 @@ node* insert(collegeType in, node* tree){
         tree = new node();
         tree->data = in;
         tree->departnumber.push_back(in.number);
-        cout << "tree is null, serial is " << tree->departnumber[0] << "\n";
+        //cout << "tree is null, serial is " << tree->departnumber[0] << "\n";
         tree->height = 1;
         tree->left = tree->right = NULL;
         return tree;
     }
 
-    cout << "in depart is " << in.department_number << "\t";
-    cout << tree->data.department_number << " tree data depart"<< "\n";
+    //cout << "in depart is " << in.department_number << "\t";
+    //cout << tree->data.department_number << " tree data depart"<< "\n";
 
     if(in.department < tree->data.department){//新增資料小於時，指派至左子樹
-        cout << "insert to left\n";
+        //cout << "insert to left\n";
         tree->left = insert(in, tree->left);
     }
     else if(in.department > tree->data.department){//新增資料大於時，指派至右子樹
-        cout << "insert to right\n";
+        //cout << "insert to right\n";
         tree->right = insert(in, tree->right);
     }
     else{//新增資料等於時，表示科系相同，則紀錄序號
-        cout << "input is the same depart, which serial is " << in.number << "\n";
+        //cout << "input is the same depart, which serial is " << in.number << "\n";
         tree->departnumber.push_back(in.number);
         return tree;
     }
 
     //新增完後，更改parent的高度
     tree->height = max(height(tree->left), height(tree->right)) + 1;
-    cout << "tree hight is " << tree->height << "\n";
+    //cout << "tree hight is " << tree->height << "\n";
 
     int bf = BF(tree);
-    cout << "BF(tree) is " << bf << "\n";
+    //cout << "BF(tree) is " << bf << "\n";
 
     if(bf > 1 && in.department < tree->left->data.department) return rr(tree);
     if(bf < -1 && in.department > tree->right->data.department) return ll(tree);
@@ -160,7 +160,7 @@ node* insert(collegeType in, node* tree){
 }
 
 node* rr(node* y){
-    cout << "need rr\n";
+    //cout << "need rr\n";
     node* x = y->left;
     node* xr = x->right;
 
@@ -174,7 +174,7 @@ node* rr(node* y){
 }
 
 node* ll(node* x){
-    cout << "need ll\n";
+    //cout << "need ll\n";
     node* y = x->right;
     node* xr = y->left;
 
@@ -188,13 +188,13 @@ node* ll(node* x){
 }
 
 node* rl(node* x){
-    cout << "need rl\n";
+    //cout << "need rl\n";
     x->right = rr(x->right);
     return ll(x);
 }
 
 node* lr(node* x){
-    cout << "need lr\n";
+    //cout << "need lr\n";
     x->left = ll(x->left);
     return rr(x);
 }
