e716b03e3ff475d5a9c5093eb6753205ae170e10
Jackyspractice <91265066+Jackyspractice@users.noreply.github.com>
2022-04-30 16:51:59 +0800

    Add files via upload

---
+++ 上機＃3/DSex3_10828152.cpp
@@ -0,0 +1,120 @@
+//10828152 ?»æ?ä¸‰ç”² å¼µæ???
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class collegeType{
+    public:
+        int serial;
+        string sid;
+        string sname;
+        int score[6];
+        float avg;
+};
+
+class DataInput{
+    public:
+        vector<collegeType> cSet;
+
+        void split(string *buffer, string line, char sp){
+            string reg[9] = {"\0"};
+            int k = 0;
+            for(int i = 0; i < line.size(); i++){
+                if(line[i] != sp){
+                    reg[k] = reg[k] + line[i];
+                }
+                else{
+                    buffer[k] = reg[k];
+                    k++;
+                }
+            }
+            buffer[8] = reg[8];
+        }
+
+        void store(int i, vector<string> buffer){
+            collegeType data;
+            data.serial = i + 1;
+            cout << "data.serial = " << data.serial << "\t";
+            data.sid = buffer[0 + i * 9];
+            data.sname = buffer[1 + i * 9];
+
+            cout << "data.sid = " << data.sid << "\t";
+            cout << "data.sname = " << data.sname << "\t";
+
+            int k = 0;
+            for(int n = 2; n < 8; n++){
+                data.score[k++] = stoi(buffer[n + i * 9]);
+            }
+            cout << "data.score[5] = " << data.score[5] << "\t";
+            data.avg = stof(buffer[8 + i * 9]);
+            cout << "data.avg = " << data.avg << "\n";
+            cout << "buffer[9] = " << buffer[9] << "\n";
+            cSet.push_back(data);
+        }
+
+        void inputtxtfile(){
+            fstream file;
+            string filename;
+            string copy1 = "input";
+            string copy2 = ".txt";
+            cout << "input a file number:";
+            cin >> filename;
+            filename = copy1 + filename + copy2;
+            file.open(filename, ios::in);
+            if (!file.is_open()){
+                cout << "There is no such file\n";
+                return;
+            }
+            cSet.clear();
+            string buffer[9];
+            vector<string> buffer_split;
+            buffer_split.clear();
+            string line;
+	int data_size = 0;
+
+            while(!file.eof()){
+                getline(file, line, '\n');
+	data_size++;
+                if (line != "\0") split(buffer, line, '\t');
+		else data_size--;
+                for(int i = 0; i < 9; i++){
+                    buffer_split.push_back(buffer[i]);
+                    //cout << "buffer size = " << buffer.size() << endl;
+                    cout << buffer[i] << "\t";
+                }
+                cout << "\n";
+                //cout << line << "\n";
+            }
+
+
+            
+            
+            for(int i = 0; i < data_size ; i++){
+                store(i, buffer_split);
+            }
+            
+        }
+};
+
+int main() {
+
+    DataInput input;
+    int command = 777;
+
+    while(command != -1){
+        cout << "Please input command[stop, mission0] = [-1. 0]:";
+        cin >> command;
+        switch(command){
+            case 0:
+                input.inputtxtfile();
+                cout << input.cSet[2].sid << input.cSet[2].sname << endl;
+                cout << "complete!!\n";
+                break;
+            default:
+                cout << "command dosent exist!\n";
+        }
+    }
+    
+}
